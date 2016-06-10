d6347cc0126d04d3e7c62431fbd60ed7c146efd5
Raymond <raymondcgillett@gmail.com>
2016-06-10 14:03:36 -0400

    Fixing format

--- spring-boot/spring-boot-module/src/test/java/org/sample/test/HelloControllerTest.java
+++ spring-boot/spring-boot-module/src/test/java/org/sample/test/HelloControllerTest.java
@@ -29,8 +29,8 @@ public class HelloControllerTest {
 
     @Test
     public void getHello() throws Exception {
-        mvc.perform( MockMvcRequestBuilders.get( "/" ).accept( MediaType.APPLICATION_JSON ) )
-                .andExpect( status().isOk() );
+        mvc.perform( MockMvcRequestBuilders.get( "/" ).accept( MediaType.APPLICATION_JSON ) ).andExpect(
+                status().isOk() );
     }
 
 }
